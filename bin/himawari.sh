2a4953ab5cc4048648d86ed413d41bc802da8539
Simon Rumble <simon@poplindata.com>
2019-05-07 16:33:31 +1000

    Add satellite image to screensaver

--- bin/himawari.sh
+++ bin/himawari.sh
@@ -3,7 +3,7 @@
 curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg >/tmp/himawari-8.jpg
 if [ "$?" = "0" ]; then
 	feh --bg-max /tmp/himawari-8.jpg
-	echo "$?"
+	mogrify -format png -adaptive-resize 1920x1080 /tmp/himawari-8.jpg
 else
 	feh --bg-max /home/simon/Pictures/kids-birds.jpg
 fi
