458d7395901428d6b9b32a0aee065f40092dc344
Simon Rumble <simon@poplindata.com>
2019-05-07 16:17:53 +1000

    Add some error checking for satellite image

--- bin/himawari.sh
+++ bin/himawari.sh
@@ -1,4 +1,9 @@
 #/bin/sh
 
 curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg >/tmp/himawari-8.jpg
-feh --bg-max /tmp/himawari-8.jpg
+if [ "$?" = "0" ]; then
+	feh --bg-max /tmp/himawari-8.jpg
+	echo "$?"
+else
+	feh --bg-max /home/simon/Pictures/kids-birds.jpg
+fi
