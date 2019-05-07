46110f7a380dd8a238a820945af628d038b92a64
Simon Rumble <simon@poplindata.com>
2019-05-07 15:13:20 +1000

    Background weather satellite

---
+++ bin/himawari.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg >/tmp/himawari-8.jpg
+feh --bg-max /tmp/himawari-8.jpg
