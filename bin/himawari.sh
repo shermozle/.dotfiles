4de3b92fbdb54af90de8edb9243ec33f6170a79f
Simon Rumble <simon@poplindata.com>
2019-05-16 11:32:48 +1000

    Update error reporting on satellite bg

--- bin/himawari.sh
+++ bin/himawari.sh
@@ -1,9 +1,15 @@
 #/bin/sh
 
-curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg >/tmp/himawari-8.jpg
+# Full disk
+#curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg >/tmp/himawari-8.jpg
+
+# Just Australia
+curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/australia_true_color.jpg >/tmp/himawari-8.jpg
 if [ "$?" = "0" ]; then
-	feh --bg-max /tmp/himawari-8.jpg
+	echo `date` "$?" >/tmp/output
+	DISPLAY=:0 feh --bg-max /tmp/himawari-8.jpg
 	mogrify -format png -adaptive-resize 1920x1080 /tmp/himawari-8.jpg
 else
-	feh --bg-max /home/simon/Pictures/kids-birds.jpg
+	echo "$?" >/tmp/output
+	DISPLAY=:0 feh --bg-max /home/simon/Pictures/kids-birds.jpg
 fi
