183bb9a7aa466bd832efe4acb6a85337a4e91d7a
Simon Rumble <simon@poplindata.com>
2019-05-17 13:28:51 +1000

    Chop down full disk image for just Australia and fill screen

--- bin/himawari.sh
+++ bin/himawari.sh
@@ -1,12 +1,14 @@
 #/bin/sh
 
 # Full disk
-#curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg >/tmp/himawari-8.jpg
+curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg >/tmp/full-disk.jpg
 
 # Just Australia
-curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/australia_true_color.jpg >/tmp/himawari-8.jpg
+#curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/australia_true_color.jpg >/tmp/himawari-8.jpg
+
 if [ "$?" = "0" ]; then
 	echo `date` "$?" >/tmp/output
+	convert /tmp/full-disk.jpg -crop 2986x1680+983+3216 -resize 1920x1080 /tmp/himawari-8.jpg
 	DISPLAY=:0 feh --bg-max /tmp/himawari-8.jpg
 	mogrify -format png -adaptive-resize 1920x1080 /tmp/himawari-8.jpg
 else
