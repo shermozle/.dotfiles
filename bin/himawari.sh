3855903c227a14a2ca097ee25f5384d6cea0a909
Simon Rumble <simon@poplindata.com>
2019-05-20 10:55:54 +1000

    Better logging

--- bin/himawari.sh
+++ bin/himawari.sh
@@ -1,17 +1,17 @@
 #/bin/sh
 
+echo >>/tmp/output
+echo `date` Starting >>/tmp/output
+
 # Full disk
 curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg >/tmp/full-disk.jpg
 
-# Just Australia
-#curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/australia_true_color.jpg >/tmp/himawari-8.jpg
-
 if [ "$?" = "0" ]; then
-	echo `date` "$?" >/tmp/output
+	echo `date` "$?" >>/tmp/output
 	convert /tmp/full-disk.jpg -crop 2986x1680+983+3216 -resize 1920x1080 /tmp/himawari-8.jpg
 	DISPLAY=:0 feh --bg-max /tmp/himawari-8.jpg
 	mogrify -format png -adaptive-resize 1920x1080 /tmp/himawari-8.jpg
 else
-	echo "$?" >/tmp/output
+	echo `date` "$?" >>/tmp/output
 	DISPLAY=:0 feh --bg-max /home/simon/Pictures/kids-birds.jpg
 fi
