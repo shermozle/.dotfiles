be7a7bb709549143c8e73e6bed339b5d2d675c85
Simon Rumble <simon@poplindata.com>
2019-05-23 13:49:41 +1000

    Improve error handling

--- bin/himawari.sh
+++ bin/himawari.sh
@@ -4,14 +4,14 @@ echo >>/tmp/output
 echo `date` Starting >>/tmp/output
 
 # Full disk
-curl -s http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg >/tmp/full-disk.jpg
+curl -sS http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg >/tmp/full-disk.jpg
 
 if [ "$?" = "0" ]; then
-	echo `date` "$?" >>/tmp/output
+	echo `date` Success "$?" >>/tmp/output
 	convert /tmp/full-disk.jpg -crop 2986x1680+983+3216 -resize 1920x1080 /tmp/himawari-8.jpg
 	DISPLAY=:0 feh --bg-max /tmp/himawari-8.jpg
 	mogrify -format png -adaptive-resize 1920x1080 /tmp/himawari-8.jpg
 else
-	echo `date` "$?" >>/tmp/output
+	echo `date` Fail "$?" >>/tmp/output
 	DISPLAY=:0 feh --bg-max /home/simon/Pictures/kids-birds.jpg
 fi
